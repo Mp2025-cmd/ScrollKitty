5218367533938b93dba1f0bfb162370511d1642c
Peter <peter@MacBook-Pro-8.local>
2025-10-19 22:24:43 -0400

    Initial Commit

---
+++ ScrollKitty/ScrollKittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollKittyApp.swift
+//  ScrollKitty
+//
+//  Created by Peter on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrollKittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrollKittyTests/ScrollKittyTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScrollKittyTests.swift
+//  ScrollKittyTests
+//
+//  Created by Peter on 10/19/25.
+//
+
+import Testing
+
+struct ScrollKittyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
