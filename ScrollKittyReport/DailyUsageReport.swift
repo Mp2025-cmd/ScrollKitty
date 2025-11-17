ceedc9d4c4e62f5434cfbb9cf8f1d9b2cbaa6d4b
mpaul <mpaul@github.com>
2025-11-16 23:53:19 -0500

    Remove debug print statements from extension

--- ScrollKittyReport/DailyUsageReport.swift
+++ ScrollKittyReport/DailyUsageReport.swift
@@ -11,21 +11,17 @@ struct DailyUsageReport: DeviceActivityReportScene {
     let content: (UsageData) -> DailyUsageReportView
 
     func makeConfiguration(representing data: DeviceActivityResults<DeviceActivityData>) async -> UsageData {
-        print("[ScrollKittyReport] makeConfiguration called")
-
         // Sum ALL app durations (NO WEBSITES)
         var totalSeconds: Double = 0
         for await segment in data.flatMap({ $0.activitySegments }) {
             for await category in segment.categories {
                 for await app in category.applications {
                     totalSeconds += app.totalActivityDuration
-                    print("[ScrollKittyReport] + \(app.totalActivityDuration)s from \(app.application.localizedDisplayName ?? "unknown")")
                 }
             }
             // Skip webDomains entirely - we don't want Safari/Chrome time
         }
 
-        print("[ScrollKittyReport] Calculated \(totalSeconds)s total (excluded websites)")
         return UsageData(totalSeconds: totalSeconds)
     }
 }
